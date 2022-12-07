732c46a413a4333d262acb8102d184f108617c63
Jack Maiden <jack.maiden@koder.ly>
2022-12-07 17:52:31 +0000

    Day 5 to 7

---
+++ AdventOfCodeSharp/Challenge/Y2022/Day05/Day05.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCodeSharp.Challenge.Y2022.Day05;
+
+[ChallengeName("Day 5: Supply Stacks")]
+public class Day05 : IChallenge
+{
+    public async Task<object> TaskPartOne(string input) => await ContainerStacks(input, false);
+
+    public async Task<object> TaskPartTwo(string input) => await ContainerStacks(input, true);
+
+    public async Task<object> ContainerStacks(string input, bool stackMove)
+    {
+
+        var challengeSplit = input.Split("\r\n\r\n");
+
+        var containers = challengeSplit[0].Split("\r\n").Reverse().ToList();
+
+        var containerIds = containers[0];
+        containers.Remove(containerIds);
+
+        var stacks = containerIds.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(container => new Stack<char>()).ToList();
+
+        foreach (var container in containers)
+        {
+            for (var i = 0; i < stacks.Count; i++)
+            {
+                var stackNumber = containerIds.IndexOf((i + 1).ToString(), StringComparison.Ordinal);
+
+                if (!char.IsWhiteSpace(container[stackNumber]))
+                {
+                    stacks[i].Push(container[stackNumber]);
+                }
+            }
+        } //Loop and add the containers to stacks
+
+        var regex = new Regex(@"move (\d+) from (\d+) to (\d+)", RegexOptions.IgnoreCase);
+
+        foreach (var command in challengeSplit[1].Split("\r\n"))
+        {
+            var x = regex.Match(command);
+
+            var count = int.Parse(x.Groups[1].Value);
+            var from = int.Parse(x.Groups[2].Value) - 1;
+            var to = int.Parse(x.Groups[3].Value) - 1;
+
+            var moveQueue = new List<char>();
+
+            for (var i = 0; i < count; i++)
+            {
+                if (stacks[from].Count < 0) continue;
+                var container = stacks[from].Pop();
+                moveQueue.Add(container);
+            }
+
+            if (stackMove) moveQueue.Reverse();
+
+            foreach (var container in moveQueue)
+            {
+                stacks[to].Push(container);
+            }
+        }
+
+        return stacks.Where(stack => stack.Count > 0).Aggregate("", (current, stack) => current + stack.Pop());
+
+    }
+
+}
---
+++ AdventOfCodeSharp/Challenge/Y2022/Day06/Day06.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCodeSharp.Challenge.Y2022.Day06;
+
+[ChallengeName("Day 6: Tuning Trouble")]
+public class Day06: IChallenge
+{
+    public async Task<object> TaskPartOne(string input) => await StartOfXMarker(input,4);
+
+    public async Task<object> TaskPartTwo(string input) => await StartOfXMarker(input, 14);
+
+
+    //start of packet marker is a 4 char unique string
+    //start of Message marker is a 14 char unique string
+
+    private async Task<object> StartOfXMarker(string input, int distinct)
+    {
+        for (var i = 0; i < input.Length - distinct; i++)
+        {
+            if (input.Substring(i, distinct).ToCharArray().GroupBy(x => x).Any(g => g.Count() > 1)) continue;
+
+            return i+ distinct;
+        }
+
+        return null;
+    }
+}
