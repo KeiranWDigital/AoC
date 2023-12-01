c18ea537fb3ad02b54dc5a10755c30dba3e545c5
Jack Maiden <jack.maiden@koder.ly>
2023-12-01 14:58:54 +0000

    Day 1 2023

---
+++ AdventOfCodeSharp/Challenge/Y2023/Day01/Day01.cs
@@ -0,0 +1,114 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCodeSharp.Challenge.Y2023.Day01;
+
+//Invoked Implicitly
+[ChallengeName("Day 1: Trebuchet?!")]
+public class Day01 : IChallenge
+{
+    public async Task<object> TaskPartOne(string input) => await onlyNumbers(input);
+    public async Task<object> TaskPartTwo(string input) => await wordsAndDigits(input);
+
+    public async Task<int> onlyNumbers(string input)
+    {
+        var lines = input.GetLines();
+
+        char[] testChars = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        return (from line in lines let minIndex = line.IndexOfAny(testChars) let maxIndex = line.LastIndexOfAny(testChars) select String.Concat(line[minIndex], line[maxIndex]) into i select int.Parse(i)).Sum();
+    }
+
+
+    public async Task<int> wordsAndDigits(string input)
+    {
+        var lines = input.GetLines();
+        string[] digits = new string[]
+        {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"
+        };
+
+        var sum = 0;
+        foreach (var line in lines)
+        {
+            
+            var minIndex = digits.Select(s => line.IndexOf(s)).Where(s => s > -1).Min();
+            var maxIndex = digits.Select(s => line.LastIndexOf(s)).Max();
+
+            char first;
+            char last;
+
+            if (char.IsDigit(line[minIndex]))
+            {
+                first = line[minIndex];
+            }
+            else
+            {
+                var s2 = line.Substring(minIndex);
+
+                var digit = digits.Where(s => s2.StartsWith(s)).FirstOrDefault();
+
+                first = digit switch
+                {
+                    "one" => '1',
+                    "two" => '2',
+                    "three" => '3',
+                    "four" => '4',
+                    "five" => '5',
+                    "six" => '6',
+                    "seven" => '7',
+                    "eight" => '8',
+                    "nine" => '9'
+                };
+            }
+
+            if (char.IsDigit(line[maxIndex]))
+            {
+                last = line[maxIndex];
+            }
+            else
+            {
+                var s2 = line.Substring(maxIndex);
+
+                var digit = digits.Where(s => s2.StartsWith(s)).FirstOrDefault();
+
+                last = digit switch
+                {
+                    "one" => '1',
+                    "two" => '2',
+                    "three" => '3',
+                    "four" => '4',
+                    "five" => '5',
+                    "six" => '6',
+                    "seven" => '7',
+                    "eight" => '8',
+                    "nine" => '9'
+                };
+            }
+
+            var i = String.Concat(first, last);
+
+            sum += int.Parse(i);
+        }
+
+        return sum;
+    }
+}
